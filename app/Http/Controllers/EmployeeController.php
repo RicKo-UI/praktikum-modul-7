008e312d5acbe720703a69a1b5f8f7a5e11240fb
RicKo-UI <juanenricoc12@gmail.com>
2023-06-21 21:04:18 +0700

    create employee index

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -13,7 +13,10 @@ class EmployeeController extends Controller
      */
     public function index()
     {
+        $pageTitle = 'Employee List';  
+        $employees = Employee::all(); 
         
+        return view('employee.index', [ 'pageTitle' => $pageTitle, 'employees' => $employees ]);
     }
 
     /**
